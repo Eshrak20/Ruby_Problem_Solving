af4a47399c259f078ecf268277e14820c806cf56
Eshrak <uemran2018@gmail.com>
2023-09-10 07:09:54 +0600

    Update the tabel

--- Multiplication_table.rb
+++ Multiplication_table.rb
@@ -14,3 +14,25 @@ end
 
 
 
+# -------------- this code will take the input from user ---------------
+
+
+=begin
+
+def multiplication_function(n, range)
+  puts "Multiples of #{n}:"
+  range.each do |number|
+    puts "#{n} x #{number} =  #{n * number}"
+  end
+  puts
+end
+puts "Enter the number"
+input = gets.chomp.to_i
+range = 1..input
+
+range.each do |multiplication_number |
+  multiplication_function(multiplication_number, range)
+end
+
+=end
+
