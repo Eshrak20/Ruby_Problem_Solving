a92b23ffe1bcf0532d3e21d5c541fac4f83976ef
Eshrak <uemran2018@gmail.com>
2023-10-07 08:08:55 +0600

    assinment 1 problem 4

--- Assingment_1.rb
+++ Assingment_1.rb
@@ -1,3 +1,5 @@
+# problem_3
+=begin
 puts "Enter an integer: "
 input = gets.chomp
 
@@ -11,3 +13,29 @@ if input.to_i.to_s == input
 else
   puts "This is not an integer Please give a valid number"
 end
+
+=end
+
+
+# problem_4
+=begin
+puts "Enter a number: "
+num = gets.chomp.to_i
+
+(1...num).each do |i|
+  if i % 3 == 0 && i % 7 == 0
+    puts i
+  end
+end
+=end
+
+
+
+
+
+
+
+
+
+
+
